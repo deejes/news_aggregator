150cc0a6f70dcc90f2e8476e966b807998e3c2fc
deejes <j.devansh29@gmail.com>
2018-01-25 16:42:14 -0800

    refactor to fully parse and stringify links in sitemap struct

--- news_agg.go
+++ news_agg.go
@@ -7,20 +7,12 @@ import ("fmt"
       )
 
 type SitemapIndex struct {
-    Locations []Location `xml:"sitemap"`
-}
-
-type Location struct {
-  Loc string `xml:"loc"`}
-
-func (l Location) String() string{
-      return fmt.Sprintf(l.Loc)
+    Locations []string `xml:"sitemap>loc"`
 }
 
 func main(){
     resp,_ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
     bytes,_ := ioutil.ReadAll(resp.Body)
-    //body_string := string(bytes)
     resp.Body.Close()
     var s SitemapIndex
     xml.Unmarshal(bytes,&s)
