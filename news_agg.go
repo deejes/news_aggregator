5f392f04e2d15bdf10a1c3ca3b41ef11e45c405f
deejes <j.devansh29@gmail.com>
2018-01-26 15:31:55 -0800

    add hello world webserver and comments to webscraping code

--- news_agg.go
+++ news_agg.go
@@ -10,11 +10,41 @@ type SitemapIndex struct {
     Locations []string `xml:"sitemap>loc"`
 }
 
+type News struct {
+    Titles []string `xml:"url>news>title"`
+    Keywords []string `xml:"url>news>keywords"`
+    Locations []string `xml:"url>loc"`
+  }
+
+type NewsMap struct{
+  Keyword string
+  Location string
+}
+
+
 func main(){
+    var s SitemapIndex
+    var n News
+    news_map := make(map[string]NewsMap) // this is map(dict) with key of type string and value of NewsMap
     resp,_ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
     bytes,_ := ioutil.ReadAll(resp.Body)
     resp.Body.Close()
-    var s SitemapIndex
-    xml.Unmarshal(bytes,&s)
-    fmt.Println(s.Locations)
+    xml.Unmarshal(bytes,&s) // so now xml has been unpacked into s of type SitemapIndex
+
+    for _,Location := range s.Locations{ // iterating over s
+        resp,_ := http.Get(Location)  // we're doing a get on each link that we got from the sitemap
+        bytes,_ := ioutil.ReadAll(resp.Body)
+        xml.Unmarshal(bytes,&n) // now from each link, results have been unpacked into news struct.
+
+        for idx,_ := range n.Titles{ // idx is simply the indexes of n.Titles
+        news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx],n.Locations[idx]} // creates a hashmap(?) with titles as keys and keyword and location as values
+        }
+    }
+
+    for idx,data := range news_map{
+    fmt.Println("\n",idx)
+    fmt.Println("\n",data.Keyword)
+    fmt.Println("\n",data.Location)
+    }
+
 }
