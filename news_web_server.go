28893987727a9840da98862e0ba0f94293218fc9
deejes <j.devansh29@gmail.com>
2018-01-26 15:42:05 -0800

    add function to handle newsAgg function

--- news_web_server.go
+++ news_web_server.go
@@ -3,14 +3,27 @@ package main
 import (
     "fmt"
     "net/http"
+    "html/template"
 )
+
+type NewsAggPage struct{
+    Title string
+    News string
+}
 
 
 func indexHandler(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "<h1>HW</h1>")
 }
 
+func newsAggHandler(w http.ResponseWriter, r *http.Request){
+    p := NewsAggPage{Title:"Title",News:"News"}
+    t, _ := template.ParseFiles("htmltemplate.html")
+    t.Execute(w,p)
+  }
+
 func main(){
       http.HandleFunc("/",indexHandler)
+      http.HandleFunc("/agg/",newsAggHandler)
       http.ListenAndServe(":8000",nil)
     }
