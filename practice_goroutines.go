bea4e164fd2ec1e079e94228b18c097ecaf54e63
deejes <j.devansh29@gmail.com>
2018-02-04 16:22:14 -0800

    add defer statement

--- practice_goroutines.go
+++ practice_goroutines.go
@@ -9,25 +9,23 @@ import (
 var wg sync.WaitGroup
 
 func say(s string){
+  defer wg.Done()
   for i:=0; i<3;i++{
     fmt.Println(s)
     time.Sleep(time.Millisecond*500)
-    
-    }
-    wg.Done()
+      }
     }
 
 
     func main() {
-      wg.Add(1)
       go say("Hey")
       wg.Add(1)
       go say("There")
-      wg.Wait()
-      
       wg.Add(1)
-      go say("Hi")
       wg.Wait()
-      
+      go say("Hi")
+      wg.Add(1)
+      wg.Wait()
+      fmt.Println("s")
     }
 
