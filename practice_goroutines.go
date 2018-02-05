d369a7ef2fbecbc21deabfcdec738112028bcb82
deejes <j.devansh29@gmail.com>
2018-02-04 23:45:01 -0800

    basic channels

--- practice_goroutines.go
+++ practice_goroutines.go
@@ -1,30 +1,21 @@
 package main
 
-import (
-  "fmt"
-)
+import "fmt"
 
-
-func cleanup(){
-  r := recover() 
-  if r!= nil{
-    fmt.Println("recoevered in cleanup",r)
-  }
+func timesFive(c chan int,n int){
+  c <- n*5
 }
 
-func hello(s string){
 
-  defer cleanup()
-  fmt.Println("hello" + s)
-  for i:=0;i < 4; i++ {
-    fmt.Println(i)
-
-    if i == 2{
-      panic ("oh no!")
-    }
-  }
+func main(){
+  num_channel := make(chan int)
+  go timesFive(num_channel, 0)
+  go timesFive(num_channel, 5)
+  go timesFive(num_channel, 10)
+  x := <-num_channel
+  y := <-num_channel
+  z := <-num_channel
+  fmt.Println(x,y,z)
+  // HOW IS THE ORDERING HAPPENING THOUGH?
 }
 
-func main() {
-  hello("ss")
-}
