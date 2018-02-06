116d8ac7fb0d861c72f7d7b0d9c3059298502e27
deejes <j.devansh29@gmail.com>
2018-02-05 22:29:12 -0800

    parse year page and grab links

--- nytime_agg.go
+++ nytime_agg.go
@@ -7,26 +7,41 @@ import (
   //"io/ioutil"
   //"golang.org/x/net/html"
   "github.com/PuerkitoBio/goquery"
-  "strconv"
+  //"strconv"
 )
 
 
-var root_url = "http://spiderbites.nytimes.com/"
+//var root_url = "http://spiderbites.nytimes.com/"
+//func main(){
+//  var yearly_links []string
+//  doc,_ := goquery.NewDocument(root_url)
+//  sel := doc.Find("h3").First().Parent().Contents()
+//  for i := range sel.Nodes {
+//    node := sel.Eq(i)
+//    node_text, _ := strconv.Atoi(node.Text())
+//    if node_text >= 2000 {
+//      url,_ := node.Attr("href")
+//      yearly_links = append(yearly_links,root_url+url)
+//    }
+//  }
+//  for ind,link := range yearly_links{
+//  fmt.Println(ind,link)
+//  }
+//}
+
+var root_url = "http://spiderbites.nytimes.com/free_2000/index.html"
+
+
 func main(){
-  var yearly_links []string
+  var parts_links []string
   doc,_ := goquery.NewDocument(root_url)
-  sel := doc.Find("h3").First().Parent().Contents()
-  for i := range sel.Nodes {
-    node := sel.Eq(i)
-    node_text, _ := strconv.Atoi(node.Text())
-    if node_text >= 2000 {
-      url,_ := node.Attr("href")
-      yearly_links = append(yearly_links,root_url+url)
-    }
+  sel := doc.Find("#mainContent").Find("li")
+  for i := range sel.Nodes{
+    node := sel.Eq(i).Children().First()
+    url,_ := node.Attr("href")
+    fmt.Println(url)
+    parts_links = append(parts_links,url)
   }
-  for ind,link := range yearly_links{
-  fmt.Println(ind,link)
-  }
+  fmt.Println(len(parts_links))
+  fmt.Println(parts_links)
 }
-
-
