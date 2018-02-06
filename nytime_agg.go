ba3821137a56c73205b87954440742a15c2e3d3d
deejes <j.devansh29@gmail.com>
2018-02-06 13:23:05 -0800

    integrate get article data and get article links functions

--- nytime_agg.go
+++ nytime_agg.go
@@ -44,47 +44,59 @@ var root_url = "http://spiderbites.nytimes.com/"
 //  fmt.Println(yearly_page_links)
 //}
 
-
-//var stories_links_page = "http://spiderbites.nytimes.com//free_2000/articles_2000_12_00002.html"
-//func main(){
-//  var story_links []string
-//  doc,_ := goquery.NewDocument(stories_links_page)
-//  sel := doc.Find("#headlines").Find("li")
-//  for i := range sel.Nodes{
-//    node := sel.Eq(i).Children().First()
-//    url,_ := node.Attr("href")
-//    story_links= append(story_links, url)
-//  }
-//  fmt.Println(story_links)
-//}
-//
-
 type Article struct{
   Title string
-  Author string
+  //Author string
   Date string
   Body string
 }
 
+var article_array []Article
+
+
 var article_page = "http://www.nytimes.com/2000/12/22/arts/art-review-party-time-inside-and-out-playful-wit-reigns-at-skidmore-s-new-museum.html"
-func main(){
+func get_article_data(url string) {
   var a Article
-  doc,_ := goquery.NewDocument(article_page)
+  doc,_ := goquery.NewDocument(url)
 
   title := doc.Find("h1")
   date := doc.Find(".dateline")
-  author := doc.Find(".byline")
+  //author := doc.Find(".byline")
   body := doc.Find(".story-body-text")
 
   a.Title = title.Text()
-  a.Author= author.Text()[3:]
+  //a.Author= author.Text()[3:]
   a.Date = date.Text()
   a.Body = body.Text()
 
   fmt.Println(a)
+  //return (a)
   //for i := range sel.Nodes{
   //node := sel.Eq(i).Children().First()
   //url,_ := node.Attr("href")
   //story_links= append(story_links, url)
   //}
 }
+
+
+var stories_links_page = "http://spiderbites.nytimes.com//free_2000/articles_2000_12_00002.html"
+func main(){
+  var story_links []string
+  doc,_ := goquery.NewDocument(stories_links_page)
+  sel := doc.Find("#headlines").Find("li")
+  // append article links to story_links slice
+  for i := range sel.Nodes{
+    node := sel.Eq(i).Children().First()
+    url,_ := node.Attr("href")
+    story_links= append(story_links, url)
+  }
+  // iterate over article links
+  for i := range story_links{
+    l := story_links[i]
+    fmt.Println(l)
+    get_article_data(l)
+  }
+
+}
+
+
